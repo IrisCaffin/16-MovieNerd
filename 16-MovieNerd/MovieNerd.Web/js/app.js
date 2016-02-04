e9bddbc18808e015da589b3db796955487f0b977
Iris Caffin <iriscaffin@gmail.com>
2016-02-03 23:14:09 -0800

    finished no extras no search resuls

---
+++ 16-MovieNerd/MovieNerd.Web/js/app.js
@@ -0,0 +1,19 @@
+﻿angular.module('app', []);
+
+angular.module('app').controller('indexController', function ($scope, $http) {
+
+    $scope.searchTerm = '';
+
+    console.log('issuing http get');
+
+    $scope.searchForMovie = function () {
+        $http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm)
+        .success(function (data) {
+            $scope.movies = data.Search;
+            console.log(data);
+        })
+        .error(function (error) {
+            alert(error);
+        });
+    };   
+});
